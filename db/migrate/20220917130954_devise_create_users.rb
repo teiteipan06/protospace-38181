42c9503130486c4013d9ccafd7938edac8c8fd81
teiteipan06 <y7321543@gmail.com>
2022-09-29 20:06:46 +0900

    user table commit

--- db/migrate/20220917130954_devise_create_users.rb
+++ db/migrate/20220917130954_devise_create_users.rb
@@ -6,6 +6,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :name,               null: false, default: ""
+      t.string :profile,            null: false, default: ""
+      t.string :occupation,         null: false, default: ""
+      t.string :position,           null: false, default: ""
 
       ## Recoverable
       t.string   :reset_password_token
