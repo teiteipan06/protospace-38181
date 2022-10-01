cfc4e130d6c0b85fbff0953aeafcaa4dc8b8acd8
teiteipan06 <y7321543@gmail.com>
2022-10-01 12:55:48 +0900

    commit

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,7 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show] 
   before_action :set_prototype, only: [:edit, :show, :update, :destroy]
-  before_action :move_to_index, only: [:edit, :update, :destroy]
+  #before_action :move_to_index, only: [:edit, :update, :destroy]
 
   def index
     @prototypes = Prototype.all
@@ -29,9 +29,9 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
-    unless user_signed_in? && current_user.id == @prototype.user_id
-      redirect_to action: :index
-    end
+    #unless user_signed_in? && current_user.id == @prototype.user_id
+      #redirect_to action: :index
+    #end
   end
 
   def update
